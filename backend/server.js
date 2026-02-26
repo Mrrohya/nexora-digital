823bff2d5a685ae169bdc330e69667065a9e0f11
Rohit Ram Vibhute <vibhute669@gmail.com>
2026-02-26 14:59:14 +0530

    Fixed port for production

--- backend/server.js
+++ backend/server.js
@@ -50,4 +50,8 @@ app.post("/contact", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("🚀 Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
